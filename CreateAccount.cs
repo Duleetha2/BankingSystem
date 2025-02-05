84fc8baf606a2e73e9bda3fd95d9fc8686784cec
Duleetha2 <148762717+Duleetha2@users.noreply.github.com>
2025-02-05 12:13:42 +0530

    Add files via upload

---
+++ CreateAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    internal class CreateAccount
+    {
+        public int AccountNumber { get; }
+        public int IdNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float InitialBalance { get; set; }
+
+        public CreateAccount(int accountNumber, int idNumber, string firstName, string lastName, float initialBalance)
+        {
+            AccountNumber = accountNumber;
+            IdNumber = idNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            InitialBalance = initialBalance;
+        }
+
+        public double AccNumber() 
+        {
+            Random random = new Random();
+            var account = random.Next(100000000,999999999);
+            return account;
+        }
+
+        public double SecretPin()
+        {
+            Random SecretPin = new Random();
+            var Pin = SecretPin.Next(1000, 1999);
+            return Pin;
+        }
+
+        public void MoneyDeposits()
+        {
+            Console.WriteLine("Enter your Deposit amount :");
+            float amount = float.Parse(Console.ReadLine());
+            InitialBalance += amount;
+            Console.WriteLine($"You succesfuly deposited {amount}. Your updated balance is {InitialBalance}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BankingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Account() 
+        {
+            Console.Write("Enter Your ID Number :");
+            int Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Your First Name:");
+            string Firstname = Console.ReadLine();
+
+            Console.Write("Enter Your Last Name:");
+            string Lastname = Console.ReadLine();
+
+            Console.Write("Enter Your Initial Balance:");
+            float InitialAmount = float.Parse(Console.ReadLine());
+        }
+    }
+}
